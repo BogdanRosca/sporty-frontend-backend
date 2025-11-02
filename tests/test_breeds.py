4107739fedda903843d3917adea9bd891d83e531
rBogdanCS <rosca.bogdan02@gmail.com>
2025-11-02 20:29:26 +0100

    add allure decorators

--- tests/test_breeds.py
+++ tests/test_breeds.py
@@ -1,14 +1,18 @@
 import utils.breeds
 from tests.base_test import BaseTest
+import allure 
 
+@allure.suite("v1/breeds")
 class TestGetDogBreeds(BaseTest):
     """Test suite for getting dog breeds details"""
 
     def test_get_all_breeds(self):
+        """Test getting all dog breeds with details"""
         response = utils.breeds.get_breeds(self.auth_headers)
         print(response)
 
 
     def test_get_specific_breed(self):
+        """Test getting specific dog breed with details"""
         response = utils.breeds.get_breed(self.auth_headers, self.breed_id)
         print(response)
