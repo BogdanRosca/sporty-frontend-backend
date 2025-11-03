d7943e880bda3f971a23d6fb911d2cfcc00bd852
rBogdanCS <rosca.bogdan02@gmail.com>
2025-11-03 18:47:43 +0100

    fix loading of envs

--- conftest.py
+++ conftest.py
@@ -1,7 +1,10 @@
 import sys
 from pathlib import Path
+from utils.auth import init_auth_env
 
 # Add the project root directory to Python path
 project_root = Path(__file__).parent
 if str(project_root) not in sys.path:
     sys.path.insert(0, str(project_root))
+
+init_auth_env()
